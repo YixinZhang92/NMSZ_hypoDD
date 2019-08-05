8fb3832e9b9b2f1685d98b217b4834bd48a9dd16
YixinZhang92 <yzhang22@memphis.edu>
2019-08-05 00:52:32 -0500

    rearrange to final use

--- preHypoDD/genPhase/phaseGenerate.m
+++ preHypoDD/genPhase/phaseGenerate.m
@@ -1,6 +1,6 @@
 % to get phase.dat for each year
-for i = 1:19
-    target_year = 1999+i
+for i = 1:20
+    target_year = 1999+i;
     if target_year <= 2014
         year = num2str(target_year)
         pre_ph2dt_arc(year)
