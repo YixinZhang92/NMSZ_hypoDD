d20f1d4e16055609dbce56c212558cb68eb9fa9c
YixinZhang92 <yzhang22@memphis.edu>
2019-08-12 21:13:04 -0500

    bugs need to fix

--- MhypoDD/preFileClusterPick.m
+++ MhypoDD/preFileClusterPick.m
@@ -1,5 +1,5 @@
-reloc = load('hypoDD.reloc');
-loc = load('hypoDD.loc');
+reloc = load('../hypoDD.reloc');
+loc = load('../hypoDD.loc');
 % set origin
 x0 = -90;
 y0 = 36.35;
@@ -8,29 +8,36 @@ d2l = cos(y0/180)*111.699; % degree to km
 reloc_lat = reloc(:,2);
 reloc_lon = reloc(:,3);
 reloc_depth = -reloc(:,4);
+reloc_ind = reloc(:,1);
 
 loc_lat = loc(:,2);
 loc_lon = loc(:,3);
 loc_depth = -loc(:,4);
+loc_ind = loc(:,1);
 
-fid = fopen('xyz.reloc','w');
+
+fid = fopen('../xyz.reloc','w');
 
 for i = 1:length(reloc_lat)
     
     x = d2l*(reloc_lon(i) - x0);
     y = d2l*(reloc_lat(i) - y0);
     z = reloc_depth(i);
-    fprintf(fid,'%6.3f %6.3f %5.2f\n', x, y, z);
+    ind = reloc_ind(i);
+    fprintf(fid,'%6.3f %6.3f %5.2f %d \n ', x, y, z, ind);
     
 end
+fclose(fid);
 
-fid = fopen('xyz.loc','w');
+fid = fopen('../xyz.loc','w');
 
 for i = 1:length(loc_lat)
     
     xo = d2l*(loc_lon(i) - x0);
     yo = d2l*(loc_lat(i) - y0);
     zo = loc_depth(i);
-    fprintf(fid,'%6.3f %6.3f %5.2f\n', xo, yo, zo);
+    indo = loc_ind(i);
+    fprintf(fid,'%6.3f %6.3f %5.2f %d \n', xo, yo, zo, indo);
     
 end
+fclose(fid);
